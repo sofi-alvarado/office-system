d366c773f7c00cf962a610a85fe5167979b57028
Sofía Alvarado <alvaradosofiaH@outlook.com>
2024-08-14 15:44:54 -0600

    Adding modal css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootswatch/dist/lux/bootstrap.min.css";
 import './index.css';
 import AppRouter from './routes/AppRouter';
 import reportWebVitals from './reportWebVitals';
-import "bootswatch/dist/lux/bootstrap.min.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
