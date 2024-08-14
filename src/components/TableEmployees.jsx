0746532a9426bf9bd13aeb9edaebb81b3ab51cf9
Sofía Alvarado <alvaradosofiaH@outlook.com>
2024-08-14 15:44:41 -0600

    Adding modal to tableEmployee

--- src/components/TableEmployees.jsx
+++ src/components/TableEmployees.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Modal from './Modal';
 
-const TableEmployee = ({data}
-) => {
+const TableEmployee = ({data, onEmployeeUpdate}) => {
+  const [editingItem, setEditingItem] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+
+  const handleEdit = (item) => {
+    setEditingItem(item);
+    setIsModalVisible(true);
+  };
+
   return (
+    <>
     <div className="table-responsive">
       <table className="table table-sm table-hover">
         <thead>
@@ -24,6 +34,12 @@ const TableEmployee = ({data}
                 <td>{item.lastname}</td>
                 <td>{item.genre}</td>
                 <td>{item.employment_area}</td>
+                <td>
+                  <div className="d-flex align-items-center">
+                    <button type="button" className="btn btn-outline-primary" onClick={() => handleEdit(item)}>Edit</button>
+                    <button type="button" className="btn btn-outline-danger mx-2">Delete</button>
+                  </div>
+                </td>
               </tr>
             ))
           ) : (
@@ -32,6 +48,21 @@ const TableEmployee = ({data}
         </tbody>
       </table>   
     </div>
+
+    {editingItem && (
+      <Modal
+        data={editingItem}
+        show={isModalVisible} 
+        itemId={editingItem}
+        onEmployeeUpdate={onEmployeeUpdate}
+        onClose={() => {
+          setEditingItem(null); 
+          setIsModalVisible(false)
+        }}
+      />
+    )}
+</>
+
   )
 }
 
