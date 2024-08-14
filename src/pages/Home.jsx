378648c8a7426ff146d1e5d1e4daf290a9e4271d
Sofía Alvarado <alvaradosofiaH@outlook.com>
2024-08-14 17:04:30 -0600

    Adding modal for adding a new employee.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,12 @@ import React, {useEffect} from 'react';
 import useEmployee from '../hooks/useEmployee';
 import TableEmployee from '../components/TableEmployees';
 
+import Modal from '../components/Modal';
+import useModal from '../hooks/useModal';
+
 const Home = () => {
   const { data: employeeData, fetchEmployees } = useEmployee();
+  const { isOpen, openModal, closeModal } = useModal();
 
   useEffect(() => {
     fetchEmployees();
@@ -11,7 +15,14 @@ const Home = () => {
 
 return (
   <>
+   <button type="button" className="btn btn-outline-primary" onClick={() => openModal()}>Agregar Empleado</button>
     <TableEmployee data={employeeData} onEmployeeUpdate={fetchEmployees}/>
+    
+    <Modal
+      show={isOpen} 
+      onEmployeeUpdate={fetchEmployees}
+      onClose={() => closeModal()}
+    />
   </>
 )
 }
