453df64069a6e83c64c6d689bbd67bce05696b50
Sofía Alvarado <alvaradosofiaH@outlook.com>
2024-08-14 18:20:29 -0600

    Craeting Navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Office Management</a>
+      </div>
+    </nav>
+  )
+};
+
+export default NavBar;
