2a7b61d5dbe10bfeb8b6c487988e973a4f565b38
Sofía Alvarado <alvaradosofiaH@outlook.com>
2024-08-15 15:03:13 -0600

    Installing popper to fix toggle issue on navbar.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "bootswatch/dist/lux/bootstrap.min.css";
+import 'bootswatch/dist/lux/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css';
 import AppRouter from './routes/AppRouter';
 import reportWebVitals from './reportWebVitals';
